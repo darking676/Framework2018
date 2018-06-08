919ebede44a0b2ea172ce84b75727e4155a9bd8e
6class <hb.6class@gmail.com>
2018-06-08 18:12:24 +0900

    frontController

--- myFrameWork03/src/com/bit/utils/FrontController.java
+++ myFrameWork03/src/com/bit/utils/FrontController.java
@@ -1,6 +1,10 @@
 package com.bit.utils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -37,31 +41,50 @@ public class FrontController extends HttpServlet {
 		// view String view.jsp
 		// redirect String redirect:re.do
 		
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("/list.do", "com.bit.controller.ListController");
+		map.put("/add.do", "com.bit.controller.AddController");
+		map.put("/insert.do", "com.bit.controller.InsertController");
+		map.put("/detail.do", "com.bit.controller.DetailController");
+		map.put("/edit.do", "com.bit.controller.EditController");
+		map.put("/update.do", "com.bit.controller.UpdateController");
+		map.put("/delete.do", "com.bit.controller.DeleteController");
+		
 		String uri=req.getRequestURI();
 		uri=uri.substring(req.getContextPath().length());
 		BitController controller=null;
 		String className="err.do";
-		if(uri.equals("/list.do")){
-			className="com.bit.controller.ListController";
+	
+		Set<String> keys = map.keySet();
+		Iterator<String> ite = keys.iterator();
+		while(ite.hasNext()){
+			String key=ite.next();
+			if(uri.equals(key)){
+				className=map.get(key);
+			}
 		}
-		if(uri.equals("/add.do")){
-			className="com.bit.controller.AddController";
-		}
-		if(uri.equals("/insert.do")){
-			className="com.bit.controller.InsertController";
-		}
-		if(uri.equals("/detail.do")){
-			className="com.bit.controller.DetailController";
-		}
-		if(uri.equals("/edit.do")){
-			className="com.bit.controller.EditController";
-		}
-		if(uri.equals("/update.do")){
-			className="com.bit.controller.UpdateController";
-		}
-		if(uri.equals("/delete.do")){
-			className="com.bit.controller.DeleteController";
-		}
+		
+//		if(uri.equals("/list.do")){
+//			className="com.bit.controller.ListController";
+//		}
+//		if(uri.equals("/add.do")){
+//			className="com.bit.controller.AddController";
+//		}
+//		if(uri.equals("/insert.do")){
+//			className="com.bit.controller.InsertController";
+//		}
+//		if(uri.equals("/detail.do")){
+//			className="com.bit.controller.DetailController";
+//		}
+//		if(uri.equals("/edit.do")){
+//			className="com.bit.controller.EditController";
+//		}
+//		if(uri.equals("/update.do")){
+//			className="com.bit.controller.UpdateController";
+//		}
+//		if(uri.equals("/delete.do")){
+//			className="com.bit.controller.DeleteController";
+//		}
 		try {
 			Class<?> clzz = Class.forName(className);
 			controller=(BitController) clzz.newInstance();
